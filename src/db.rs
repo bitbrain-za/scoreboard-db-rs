a9e24df7184b5c87f1865350bf9b81f139850982
phil <phil@bitbrain.co.za>
2023-10-01 18:25:54 +0200

    fixes query

--- src/db.rs
+++ src/db.rs
@@ -63,7 +63,7 @@ impl Db {
         all: bool,
     ) -> std::result::Result<Vec<Score>, Box<dyn std::error::Error>> {
         let query = match all {
-            false => format!("SELECT hash, name, command, MIN(time_ns) as min_time_ns FROM {} GROUP BY name, command ORDER BY min_time_ns ASC", self.table),
+            false => format!("SELECT hash, name, command, MIN(time_ns) as min_time_ns FROM {} GROUP BY hash, name, command ORDER BY min_time_ns ASC", self.table),
             true => format!("SELECT hash, name, command, time_ns FROM {} ORDER BY time_ns ASC", self.table), 
         };
 
