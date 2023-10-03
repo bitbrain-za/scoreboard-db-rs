795647636d12d4bd48f84cb7aa12add8173b9368
phil <phil@bitbrain.co.za>
2023-10-03 21:13:26 +0200

    Adds filters for languages

--- src/filter.rs
+++ src/filter.rs
@@ -14,8 +14,10 @@ pub enum Filter {
     Bottom(usize),
     Player(Vec<String>),
     Binary(Vec<String>),
+    Language(Vec<String>),
     UniquePlayers,
     UniqueBinaries,
+    UniqueLanguages,
     Sort(SortColumn),
 }
 
@@ -26,6 +28,7 @@ impl PartialEq for Filter {
             (Filter::Binary(a), Filter::Binary(b)) => a == b,
             (Filter::UniquePlayers, Filter::UniquePlayers) => true,
             (Filter::UniqueBinaries, Filter::UniqueBinaries) => true,
+            (Filter::UniqueLanguages, Filter::UniqueBinaries) => true,
             (Filter::Sort(a), Filter::Sort(b)) => a == b,
 
             /* only allow one of these */
@@ -58,40 +61,48 @@ impl Builder {
         }
     }
 
-    pub fn add_filter(mut self, filter: Filter) -> Self {
+    fn push(filters: &mut Vec<Filter>, filter: Filter) {
         trace!("adding filter: {:?}", filter);
         match filter.clone() {
             Filter::Player(players) => {
-                let current = self.filters.iter().find(|f| matches!(f, Filter::Player(_)));
+                let current = filters.iter().find(|f| matches!(f, Filter::Player(_)));
                 if let Some(Filter::Player(current)) = current {
                     let mut players = players;
                     players.extend_from_slice(current);
                     players.dedup();
-                    self.filters.retain(|f| !matches!(f, Filter::Player(_)));
-                    self.filters.push(Filter::Player(players));
+                    filters.retain(|f| !matches!(f, Filter::Player(_)));
+                    filters.push(Filter::Player(players));
                 } else {
-                    self.filters.push(filter);
+                    filters.push(filter);
                 }
             }
             Filter::Binary(binaries) => {
-                let current = self.filters.iter().find(|f| matches!(f, Filter::Binary(_)));
+                let current = filters.iter().find(|f| matches!(f, Filter::Binary(_)));
                 if let Some(Filter::Binary(current)) = current {
                     let mut binaries = binaries;
                     binaries.extend_from_slice(current);
                     binaries.dedup();
-                    self.filters.retain(|f| !matches!(f, Filter::Binary(_)));
-                    self.filters.push(Filter::Binary(binaries));
+                    filters.retain(|f| !matches!(f, Filter::Binary(_)));
+                    filters.push(Filter::Binary(binaries));
                 } else {
-                    self.filters.push(filter);
+                    filters.push(filter);
                 }
             }
             _ => {
-                self.filters.retain(|f| *f != filter);
-                self.filters.push(filter);
+                filters.retain(|f| *f != filter);
+                filters.push(filter);
             }
         }
-        trace!("filters: {:?}", self.filters);
+        trace!("filters: {:?}", filters);
+    }
+
+    pub fn add_filter(mut self, filter: Filter) -> Self {
+        Self::push(&mut self.filters, filter);
         self
+    }
+
+    pub fn append(&mut self, filter: Filter) {
+        Self::push(&mut self.filters, filter);
     }
 
     pub fn remove_filter(mut self, filter: Filter) -> Self {
@@ -118,8 +129,10 @@ impl Collection {
                 Filter::Top(_) | Filter::Bottom(_) => Self::apply_top(&results, filter),
                 Filter::UniquePlayers => Self::apply_unique_players(&results),
                 Filter::UniqueBinaries => Self::apply_unique_binaries(&results),
+                Filter::UniqueLanguages => Self::apply_unique_languages(&results),
                 Filter::Player(players) => Self::apply_players(&results, players),
                 Filter::Binary(binaries) => Self::apply_binaries(&results, binaries),
+                Filter::Language(language) => Self::apply_languages(&results, language),
                 Filter::Sort(column) => Self::apply_sort(&results, column),
             }
         }
@@ -151,7 +164,14 @@ impl Collection {
     fn apply_unique_binaries(scores: &[Score]) -> Vec<Score> {
         trace!("applying unique binaries filter");
         let mut scores = scores.to_vec();
-        scores.dedup_by(|a, b| a.name == b.name);
+        scores.dedup_by(|a, b| a.command == b.command);
+        scores
+    }
+
+    fn apply_unique_languages(scores: &[Score]) -> Vec<Score> {
+        trace!("applying unique language filter");
+        let mut scores = scores.to_vec();
+        scores.dedup_by(|a, b| a.command == b.command);
         scores
     }
 
@@ -166,6 +186,13 @@ impl Collection {
         trace!("applying binaries filter: {:?}", binaries);
         let mut scores = scores.to_vec();
         scores.retain(|s| binaries.contains(&s.command));
+        scores
+    }
+
+    fn apply_languages(scores: &[Score], languages: &[String]) -> Vec<Score> {
+        trace!("applying language filter: {:?}", languages);
+        let mut scores = scores.to_vec();
+        scores.retain(|s| languages.contains(&s.language));
         scores
     }
 
